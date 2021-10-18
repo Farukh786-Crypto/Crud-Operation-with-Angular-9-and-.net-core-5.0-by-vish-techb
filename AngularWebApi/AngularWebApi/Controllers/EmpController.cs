ab710c4d6b5f0467de90f3988364a7a5e82dc744
Farukh786-Crypto <shaikhfarukh600@gmail.com>
2021-10-18 07:28:10 +0530

    Add files

---
+++ AngularWebApi/AngularWebApi/Controllers/EmpController.cs
@@ -0,0 +1,66 @@
+﻿using AngularWebApi.Model;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularWebApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [EnableCors("MyCorsPolicy")]
+    public class EmpController : ControllerBase
+    {
+        private readonly DatabaseContext context;
+
+        public EmpController(DatabaseContext _context)
+        {
+            this.context = _context;
+        }
+
+       [HttpGet]
+        public ActionResult<List<Employee>> Get()
+        {
+             return Ok(context.Employees.ToList<Employee>());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Employee> Get(int id)
+        {
+            var employee=context.Employees.FirstOrDefault<Employee>(a=>a.id==id);
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public ActionResult<Employee> Post(Employee employee)
+        {
+            context.Employees.Add(employee);
+            context.SaveChanges();
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        public ActionResult<Employee> Put(Employee employee)
+        {
+            var employeeInDb= context.Employees.FirstOrDefault<Employee>(a => a.id ==employee.id);
+            employeeInDb.name = employee.name;
+            employeeInDb.email = employee.email;
+            employeeInDb.password = employee.password;
+            context.SaveChanges();
+            return Ok(employee);
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<Employee> Delete(int id)
+        {
+            var employeeInDb = context.Employees.FirstOrDefault<Employee>(a=>a.id==id);
+            context.Remove<Employee>(employeeInDb);
+            context.SaveChanges();
+            return Ok(employeeInDb);
+        }
+    }
+}
---
+++ AngularWebApi/AngularWebApi/Migrations/20211016150326_add_category.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AngularWebApi.Migrations
+{
+    public partial class add_category : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isactive = table.Column<int>(type: "int", nullable: false),
+                    isdelete = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
